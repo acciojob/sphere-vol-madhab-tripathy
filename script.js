206acc75546e26c0b2442a9912afaf03cf9a12a5
Madhab Tripathy <66151264+madhab-tripathy@users.noreply.github.com>
2023-05-19 22:29:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function volume_sphere() {
     //Write your code here
-  
+	let radius = document.getElementById("radius").value;
+	let vol = document.getElementById("volume");
+	let ans = (4/3)*Math.PI*Math.pow(radius,3);
+	vol.value = ans;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
